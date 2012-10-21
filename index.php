ab79ff1550364d7c5a6c3c1d8da562d9b1ca4835
Goddchen <goddchen@googlemail.com>
2012-10-21 13:51:14 +0200

    added css file, added interval selector

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 ?>
 <html>
 <head>
+	<link rel="stylesheet" type="text/css" href="style.css">
 	<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
 	<script type="text/javascript">
 		$(document).ready(function() {
@@ -15,20 +16,31 @@
 						$("#posts").text("");
 						$.each(data.items, function(i, item) {
 							$("#posts").append(
-								item.published + "<br>"
+								"<div class='post'>"
+								+ item.published + "<br>"
 								+ "<a href='" + item.actor.url + "'><img src='" + item.actor.image.url + "'></a><br>"
 								+ "<a href='" + item.actor.url + "'>" + item.actor.displayName + "</a><br><br>"
-								+ item.object.content + "<br><br><hr><br><br>");
+								+ item.object.content + "<br><br><hr><br><br></div>");
 						});
 				});
 			}
-			setInterval(updatePosts, 60*1000);
+			var interval = setInterval(updatePosts, 60*1000);
 			updatePosts();
+			$("#interval").change(function() {
+			clearInterval(interval);
+				interval = setInterval(updatePosts, 1000*$(this).val());
+			});
 		});
 	</script>
 </head>
 <body>
 	<h1>Google+ Wall for "<?php echo($query); ?>"</h1>
+	<p>
+		Update Interval: <select id="interval">
+			<option value="10">10 Seconds</option>
+			<option value="60" selected>1 Minute</option>
+		</select>
+	</p>
 	<div id="posts" />
 </body>
 </html>
